e967dfee73bbae8e3d9cf3e721019f9e9828fa6c
Ghost <jessiecodes@gmail.com>
2016-11-18 21:40:56 -0500

    now bot only forges northeast

--- src/MyBot.rb
+++ src/MyBot.rb
@@ -1,11 +1,32 @@
 $:.unshift(File.dirname(__FILE__))
 require 'networking'
 
-def calculateMove(location,site)
+def getWeakestDirection(map,loc,site)
+  north = map.site(loc, :north)
+  east = map.site(loc, :east)
+  south = map.site(loc, :south)
+  west = map.site(loc, :west)
+  neighbors = [north.strength, east.strength, south.strength, west.strength]
+  
+  if(neighbors.min == north.strength) then return :north end
+  if(neighbors.min == east.strength)  then return :east end 
+  if(neighbors.min == south.strength) then return :south end
+  if(neighbors.min == west.strength)  then return :west end
+
+
+  return GameMap::DIRECTIONS.shuffle.first
+end
+
+def nextMove()
+  if(Random.rand() > 0.5) then return :north end
+  return :east
+end
+
+def calculateMove(map,location,site)
   if(site.strength > site.production * 2)
-    Move.new(location, GameMap::DIRECTIONS.shuffle.first)
+    Move.new(location, nextMove())
   else
-    #:still, cant figure out how to used named arrays
+    #Dont move if you arent strong
     Move.new(location, :still)
   end
 
@@ -26,7 +47,7 @@ while true
       site = map.site(loc)
 
       if site.owner == tag
-        moves << calculateMove(loc, site) 
+        moves << calculateMove(map, loc, site) 
       end
     end
   end
